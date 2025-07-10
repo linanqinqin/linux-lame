c5c2c9dd166b48be828db1299831c5639e48a41f
Nanqinqin Li <linanqinqin@princeton.edu>
2025-07-10 17:10:19 -0400

    try a helper function to obtain lame_tls_data

--- arch/x86/entry/vdso/vlame.c
+++ arch/x86/entry/vdso/vlame.c
@@ -10,25 +10,33 @@
 /* Thread-local storage for LAME data */
 __thread struct lame_thread_data lame_tls_data __attribute__((visibility("hidden")));
 
+/* Helper function to get TLS data pointer */
+static inline struct lame_thread_data *get_lame_tls_data(void) {
+    return &lame_tls_data;
+}
+
 __attribute__((naked)) void __vdso_lame_entry(void) {
     asm volatile(
         /* Save registers we'll use */
         "pushq %rax\n"
         "pushq %rcx\n"
         "pushq %rdx\n"
+        "pushq %rsi\n"
+        "pushq %rdi\n"
         
-        /* Get current thread's lame_tls_data via TLS */
-        /* On x86-64, TLS is accessed via %fs segment */
-        "movq %fs:0, %rax\n"           /* Get TLS base address */
-        "addq $lame_tls_data@tpoff, %rax\n"  /* Add offset to lame_tls_data */
+        /* Call helper function to get TLS data pointer */
+        "call get_lame_tls_data\n"
+        "movq %rax, %rsi\n"  /* Save TLS data pointer in rsi */
         
         /* Step 1: Take current lame_count and put it in r13 */
-        "movq 144(%rax), %r13\n"        /* lame_count is at offset 144 in lame_thread_data */
+        "movq 144(%rsi), %r13\n"        /* lame_count is at offset 144 */
         
         /* Step 2: Put value from r12 into lame_count */
-        "movq %r12, 144(%rax)\n"
+        "movq %r12, 144(%rsi)\n"
         
         /* Restore registers */
+        "popq %rdi\n"
+        "popq %rsi\n"
         "popq %rdx\n"
         "popq %rcx\n"
         "popq %rax\n"
@@ -39,11 +47,11 @@ __attribute__((naked)) void __vdso_lame_entry(void) {
 }
 
 int __vdso_lame_add(int x, int y) {
-	int last_commit = 0x95a9354; // the SHA for last commit so that userspace knows which version is invoked 
-	return x+y+last_commit;
+    int last_commit = 0x95a9354; // the SHA for last commit so that userspace knows which version is invoked 
+    return x+y+last_commit;
 }
 
 int lame_add(int, int)
-	__attribute__((weak, alias("__vdso_lame_add")));
+    __attribute__((weak, alias("__vdso_lame_add")));
 
 /* end */
