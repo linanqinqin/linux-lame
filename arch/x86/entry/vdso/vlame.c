d2bafe56b1eb85f721173ec4cc78622ada7bb01e
Nanqinqin Li <linanqinqin@princeton.edu>
2025-07-10 17:13:46 -0400

    try with GOT to get the tls offset

--- arch/x86/entry/vdso/vlame.c
+++ arch/x86/entry/vdso/vlame.c
@@ -10,33 +10,26 @@
 /* Thread-local storage for LAME data */
 __thread struct lame_thread_data lame_tls_data __attribute__((visibility("hidden")));
 
-/* Helper function to get TLS data pointer */
-static inline struct lame_thread_data *get_lame_tls_data(void) {
-    return &lame_tls_data;
-}
-
 __attribute__((naked)) void __vdso_lame_entry(void) {
     asm volatile(
         /* Save registers we'll use */
         "pushq %rax\n"
         "pushq %rcx\n"
         "pushq %rdx\n"
-        "pushq %rsi\n"
-        "pushq %rdi\n"
         
-        /* Call helper function to get TLS data pointer */
-        "call get_lame_tls_data\n"
-        "movq %rax, %rsi\n"  /* Save TLS data pointer in rsi */
+        /* Get current thread's lame_tls_data via TLS */
+        /* For shared objects, we need to use a different approach */
+        /* We'll use the GOT to get the TLS offset, then access via %fs */
+        "movq lame_tls_data@GOTTPOFF(%%rip), %rax\n"  /* Get TLS offset */
+        "addq %fs:0, %rax\n"                         /* Add TLS base */
         
         /* Step 1: Take current lame_count and put it in r13 */
-        "movq 144(%rsi), %r13\n"        /* lame_count is at offset 144 */
+        "movq 144(%rax), %r13\n"        /* lame_count is at offset 144 in lame_thread_data */
         
         /* Step 2: Put value from r12 into lame_count */
-        "movq %r12, 144(%rsi)\n"
+        "movq %r12, 144(%rax)\n"
         
         /* Restore registers */
-        "popq %rdi\n"
-        "popq %rsi\n"
         "popq %rdx\n"
         "popq %rcx\n"
         "popq %rax\n"
