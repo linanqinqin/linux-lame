b2e96d4df902544bfa1c6807209c6a6d477673a8
Nanqinqin Li <linanqinqin@princeton.edu>
2025-07-15 22:57:21 -0400

    adjust field types: bit-field not supporting offsetof

--- arch/x86/include/asm/vdso/lame_data.h
+++ arch/x86/include/asm/vdso/lame_data.h
@@ -22,12 +22,11 @@ struct lame_ctx {
     uint64_t r13, r14, r15;
     
     /* Status flags */
-    uint32_t valid:1;      /* Context is valid */
-    uint32_t in_use:1;     /* Context is currently in use */
-    uint32_t reserved:30;
+    uint32_t valid;      /* Context is valid */
+    uint32_t in_use;     /* Context is currently in use */
     
     /* Padding for cache alignment */
-    uint32_t padding[3];
+    uint32_t padding[2];
 } __attribute__((packed, aligned(64)));
 
 /* LAME bundle handle - manages coroutine switching */
@@ -35,19 +34,19 @@ struct lame_ctx {
 
 struct lame_handle {
     /* Active coroutine index */
-    uint8_t active;
+    uint32_t active;
     
     /* Padding for alignment */
-    uint8_t reserved[7];
-    
-    /* Context storage for coroutines */
-    struct lame_ctx ctx[LAME_COROUTINE_DEPTH];
+    uint32_t reserved[1];
     
     /* Bundle metadata */
     uint64_t bundle_id;           /* Unique bundle identifier */
     uint64_t last_switch_tsc;     /* TSC of last context switch */
     uint32_t switch_count;        /* Number of switches in this bundle */
     uint32_t cpu_id;              /* CPU this bundle is assigned to */
+    
+    /* Context storage for coroutines */
+    struct lame_ctx ctx[LAME_COROUTINE_DEPTH];
 } __attribute__((packed, aligned(64)));
 
 #define MAX_CPU_CORES 256  /* Or whatever maximum */
