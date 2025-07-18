0c759918067d1ce3a444d467dd9d519e32f80254
Nanqinqin Li <linanqinqin@princeton.edu>
2025-07-17 21:28:50 -0400

    new static va for lame handler

--- arch/x86/include/asm/lame.h
+++ arch/x86/include/asm/lame.h
@@ -5,7 +5,7 @@
 #define _ASM_X86_LAME_H
 
 /* The virtual addr of the lame handler in user VA */
-#define asm_exc_lame ((const void *)0x7ffff7fc4680UL)
+#define asm_exc_lame ((const void *)0x7ffff7fc46d0UL)
 
 #endif
 
