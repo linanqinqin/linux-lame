673ccc65aab311d366ea76b19f9891adce472cff
Nanqinqin Li <linanqinqin@princeton.edu>
2025-07-20 22:33:52 -0400

    testing/lame: add Makefile; use lame_data.h

--- tools/testing/lame/write_active_print.c
+++ tools/testing/lame/write_active_print.c
@@ -2,32 +2,10 @@
 #include <stdlib.h>
 #include <unistd.h>
 #include <sys/mman.h>
+#include <arch/x86/include/asm/vdso/lame_data.h>
 
 // External declaration of the VDSO symbol
 extern struct lame_handle lame_handle_array[];
-
-// Structure definitions (matching the C header)
-struct lame_ctx {
-    uint64_t rsp;
-    uint64_t rip;
-    uint64_t rflags;
-    uint64_t rax, rbx, rcx, rdx;
-    uint64_t rsi, rdi, rbp, r8;
-    uint64_t r9, r10, r11, r12;
-    uint64_t r13, r14, r15;
-    uint32_t valid;
-    uint32_t in_use;
-    uint32_t padding[2];
-} __attribute__((packed, aligned(64)));
-
-struct lame_handle {
-    uint64_t active;
-    uint64_t bundle_id;
-    uint64_t last_switch_tsc;
-    uint32_t switch_count;
-    uint32_t cpu_id;
-    struct lame_ctx ctx[2];  // LAME_COROUTINE_DEPTH = 2
-} __attribute__((packed, aligned(64)));
 
 int main() {
     printf("Program A: VDSO Write Test\n");
