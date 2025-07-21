81d69f644896a15ccce60d6715a0af72de991ec4
Nanqinqin Li <linanqinqin@princeton.edu>
2025-07-20 23:31:43 -0400

    testing/lame: use abs addr for lame_handle_array

--- tools/testing/lame/print_active.c
+++ tools/testing/lame/print_active.c
@@ -1,16 +1,18 @@
 #include <stdio.h>
-#include <stdlib.h>
+#include <stdint.h>
 #include <unistd.h>
 #include <sys/mman.h>
 #include <string.h>
-#include <asm/vdso/lame_data.h>
+
+#define LAME_HANDLE_ARRAY_VDSO_ADDR 0x7ffff7fa76c0ULL
 
 int main() {
     char input[10];
+    volatile uint64_t *active = (uint64_t *)LAME_HANDLE_ARRAY_VDSO_ADDR;
     
-    printf("Program B: VDSO Read Test\n");
-    printf("lame_handle_array address: %p\n", (void*)lame_handle_array);
-    printf("Initial lame_handle_array[0].active: %lu\n", lame_handle_array[0].active);
+    printf("Program B: VDSO Read Test (using hardcoded vDSO address)\n");
+    printf("lame_handle_array[0].active (vDSO): %p\n", (void*)active);
+    printf("Initial lame_handle_array[0].active: %lu\n", *active);
     
     printf("\nCommands:\n");
     printf("  'read' - Print current value\n");
@@ -31,7 +33,7 @@ int main() {
             break;
         }
         
-        printf("Current lame_handle_array[0].active: %lu\n", lame_handle_array[0].active);
+        printf("Current lame_handle_array[0].active: %lu\n", *active);
     }
     
     return 0;
--- tools/testing/lame/write_active_print.c
+++ tools/testing/lame/write_active_print.c
@@ -1,13 +1,15 @@
 #include <stdio.h>
-#include <stdlib.h>
+#include <stdint.h>
 #include <unistd.h>
 #include <sys/mman.h>
-#include <asm/vdso/lame_data.h>
+
+#define LAME_HANDLE_ARRAY_VDSO_ADDR 0x7ffff7fa76c0ULL
 
 int main() {
-    printf("Program A: VDSO Write Test\n");
-    printf("lame_handle_array address: %p\n", (void*)lame_handle_array);
-    printf("Initial lame_handle_array[0].active: %lu\n", lame_handle_array[0].active);
+    volatile uint64_t *active = (uint64_t *)LAME_HANDLE_ARRAY_VDSO_ADDR;
+    printf("Program A: VDSO Write Test (using hardcoded vDSO address)\n");
+    printf("lame_handle_array[0].active (vDSO): %p\n", (void*)active);
+    printf("Initial lame_handle_array[0].active: %lu\n", *active);
     
     printf("\nPress Enter to trigger int 0x1f (writes 99 to lame_handle_array[0].active)...\n");
     getchar();
@@ -15,12 +17,12 @@ int main() {
     // Trigger the interrupt
     __asm__ volatile("int $0x1f");
     
-    printf("After int 0x1f, lame_handle_array[0].active: %lu\n", lame_handle_array[0].active);
+    printf("After int 0x1f, lame_handle_array[0].active: %lu\n", *active);
     
-    if (lame_handle_array[0].active == 99) {
+    if (*active == 99) {
         printf("SUCCESS: Value was written correctly!\n");
     } else {
-        printf("FAILURE: Expected 99, got %lu\n", lame_handle_array[0].active);
+        printf("FAILURE: Expected 99, got %lu\n", *active);
     }
     
     return 0;
