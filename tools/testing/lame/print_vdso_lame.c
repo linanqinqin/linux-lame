fd64554271798bf78fffde687f2bff64bd7ef11b
Nanqinqin Li <linanqinqin@princeton.edu>
2025-07-25 20:36:11 -0400

    testing/lame: print __vdso_nop_entry

--- tools/testing/lame/print_vdso_lame.c
+++ tools/testing/lame/print_vdso_lame.c
@@ -4,16 +4,20 @@
 #include <link.h>      // for AT_SYSINFO_EHDR
 #include <sys/auxv.h>
 extern void __vdso_lame_entry(void);
+extern void __vdso_nop_entry(void);
 
 int main(void) {
     // Method A: via dlsym
     void *h = dlopen("linux-vdso.so.1", RTLD_NOW);
     void *sym = dlsym(h, "__vdso_lame_entry");
     printf("via dlsym:    __vdso_lame_entry = %p\n", sym);
+    sym = dlsym(h, "__vdso_nop_entry");
+    printf("via dlsym:    __vdso_nop_entry = %p\n", sym);
     sym = dlsym(h, "lame_handle_array");
     printf("via dlsym:    lame_handle_array = %p\n", sym);
 
     printf("via direct print: __vdso_lame_entry = %p\n", (void *)__vdso_lame_entry);
+    printf("via direct print: __vdso_nop_entry = %p\n", (void *)__vdso_nop_entry);
 
     // Method B: via getauxval + symbol offset
     Elf64_Ehdr *vdso_base = (Elf64_Ehdr *)getauxval(AT_SYSINFO_EHDR);
