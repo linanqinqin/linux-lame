e9080c0ba3fe8f6358c5a032a329801268bf8d40
Nanqinqin Li <linanqinqin@princeton.edu>
2025-08-04 15:48:02 -0400

    [scripts] install only modules in arch/x86/kernel

--- scripts/compile-kernel.sh
+++ scripts/compile-kernel.sh
@@ -6,6 +6,7 @@ echo "[*] Building the kernel..."
 make -j$(nproc)
 
 echo "[*] Installing modules and kernel..."
-sudo make modules_install install
+sudo make M=arch/x86/kernel modules_install 
+sudo make install
 
 echo "[*] Done!"
