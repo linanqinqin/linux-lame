90c390fc7012c91236d97748bfe59a700e54e1a9
Nanqinqin Li <linanqinqin@princeton.edu>
2025-08-04 13:30:28 -0400

    nit

--- arch/x86/include/asm/lame.h
+++ arch/x86/include/asm/lame.h
@@ -5,7 +5,7 @@
 #define _ASM_X86_LAME_H
 
 /* The virtual addr of the lame handler in user VA */
-#define asm_exc_lame ((const void *)0x555555575af0UL) 
+#define asm_exc_lame ((const void *)0x555555575a80UL) 
 #define asm_exc_nop ((const void *)0x7ffff7fc4800UL) 
 
 #endif
