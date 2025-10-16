e915ca7691ac4418a0ab52875f0a43f82375dfb3
Nanqinqin Li <linanqinqin@princeton.edu>
2025-10-16 12:36:39 -0400

    [tools/lame] counter reads stall_emulation

--- tools/lame/lame.c
+++ tools/lame/lame.c
@@ -243,8 +243,10 @@ int enable_lame(pid_t pid, int cpu_start, int cpu_end, uint64_t sample_periods, 
 
     struct lame_counter cntr_vals_end;
     get_lame_counter(&cntr_vals_end);
-    fprintf(stdout, "LAME counted: %llu, %llu\n", cntr_vals_end.nmi_entry - cntr_vals_start.nmi_entry, 
-            cntr_vals_end.handler_upcall - cntr_vals_start.handler_upcall);
+    fprintf(stdout, "LAME counted: %llu, %llu, %llu\n", 
+            cntr_vals_end.nmi_entry - cntr_vals_start.nmi_entry, 
+            cntr_vals_end.handler_upcall - cntr_vals_start.handler_upcall,
+            cntr_vals_end.stall_emulation - cntr_vals_start.stall_emulation);
 
     return 0;
 }
@@ -256,7 +258,7 @@ int print_lame_counter(void)
         fprintf(stderr, "get_lame_counter failed\n");
         return -1;
     }
-    fprintf(stdout, "LAME counter values: %llu, %llu\n", cntr_vals.nmi_entry, cntr_vals.handler_upcall);
+    fprintf(stdout, "LAME counter values: %llu, %llu, %llu\n", cntr_vals.nmi_entry, cntr_vals.handler_upcall, cntr_vals.stall_emulation);
     return 0;
 }
 
